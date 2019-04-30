6c55ff0554fd53cc7eddeb7a573f6628d8b187a6
Ellie King <ellie.king@digital.cabinet-office.gov.uk>
2019-04-30 12:09:59 +0100

    end to end test passing for get_content_store_data
    ignore .DS_Store and .idea

---
+++ src/utils/text_preprocessing.py
@@ -0,0 +1,60 @@
+from bs4 import BeautifulSoup
+
+
+def is_html(text):
+    """
+    Checks whether text is html or not
+    :param text: string
+    :return: bool
+    """
+    try:
+        return bool(BeautifulSoup(text, "html.parser").find())
+    # might be fine to except all exceptions here, as it's a low-level function
+    except Exception:
+        pass
+
+
+def extract_links_from_html(text):
+    """
+    Grab any GOV.UK domain-specific links from page text (looks for a href tags)
+    :param text: html
+    :return: list of page_paths (empty list if there are no links)
+    """
+    links = []
+    try:
+        soup = BeautifulSoup(text, "html5lib")
+        links = [link.get('href') for link in soup.findAll('a', href=True)]
+    # might be fine to except all exceptions here, as it's a low-level function
+    except Exception:
+        None
+    return [link.replace("https://www.gov.uk/", "/") for link in links
+            if (link.startswith("/") or link.startswith("https://www.gov.uk/")) and not(link.startswith('/government/uploads/system/uploads/attachment_data/file/'))]
+
+
+def extract_links_from_content_details(data, links=[]):
+    """
+    Recurses through lists and dicts to find html and then extract links
+    :param data: This function can accept a nested list or dict, or string
+    :param links: list of page_paths
+    :return:
+    """
+    if type(data) == list:
+        [extract_links_from_content_details(item, links) for item in data]
+
+    elif type(data) == dict:
+        extract_links_from_content_details(list(data.values()), links)
+
+    elif is_html(data):
+            links.extend(extract_links_from_html(data))
+
+    return links
+
+
+def clean_page_path(page_path, sep='#'):
+    """
+    We want to remove double slashes and replace them with single slashes, and we only want the page_path before a #
+    :param page_path:
+    :param sep: default '#' that we want to get rid of, we use rsplit to take away the right-most part of the string
+    :return: a nice clean page_path, more hope of matching to our content ID mapping
+    """
+    return page_path.replace('//', '/').rsplit(sep, 1)[0]
