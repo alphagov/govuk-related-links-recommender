c7446a7f5317871e645b665240580d4d718d8d92
Suganya Sivaskantharajah <suganya.sivaskantharajah@digital.cabinet-office.gov.uk>
2019-05-03 17:03:23 +0100

    create content ID node ID mappings, and test them

--- tests/unit/features/test_make_structural_network.py
+++ tests/unit/features/test_make_structural_network.py
@@ -1,3 +1,4 @@
+import json
 import pytest
 
 import pandas as pd
@@ -23,7 +24,22 @@ from src.features.make_structural_network import *
 
 
 def test_make_structural_network():
+    edges_df, content_id_node_id_mapping, node_id_content_id_mapping = make_structural_network(
+            pd.read_csv('tests/unit/fixtures/all_links_test_sample.csv'))
+
     pd_testing.assert_frame_equal(
-        make_structural_network(
-            pd.read_csv('tests/unit/fixtures/all_links_test_sample.csv')).reset_index(drop=True),
+        edges_df.reset_index(drop=True),
         pd.read_csv('tests/unit/fixtures/structural_network_test_sample.csv'))
+
+    with open(
+            'tests/unit/fixtures/content_id_node_id_mapping.json',
+            'r') as file_1:
+        assert content_id_node_id_mapping == json.load(file_1)
+
+    # saving an integer-keyed dict down as a json converted the ints to
+    # strings, so need to convert them back when comparing
+    with open(
+            'tests/unit/fixtures/node_id_content_id_mapping.json',
+            'r') as file_2:
+        assert node_id_content_id_mapping == dict(
+            (int(k), v) for k, v in json.load(file_2).items())
