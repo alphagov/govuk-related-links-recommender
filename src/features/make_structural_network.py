7f983e8f5f4ecfcac55d981e243301a8f80f2391
Suganya Sivaskantharajah <suganya.sivaskantharajah@digital.cabinet-office.gov.uk>
2019-05-07 12:09:12 +0100

    mappings for test_make_structural_network tests
    ignore tmp folders
    
    rename logging and a fn

--- src/features/make_structural_network.py
+++ src/features/make_structural_network.py
@@ -1,44 +1,27 @@
+import json
 import logging.config
-import re
+import os
+
+import pandas as pd
 
 # link types we want to use for our structural network
 STRUCTURAL_LINK_TYPES = ['embedded_links',  'collection_links', 'related_links']
+logging.config.fileConfig('src/logging.conf')
 
 
-# def str_array_to_array(x):
-#     return [l.strip() for l in re.sub(r"[\[\]']", "", x).split(",")]
-#
-#
-# def get_links_for_each_page(all_links):
-#     """Get the base_paths? contentIDs? of links we care about for each page (structural data of the site)
-#
-#     get links where their type is in TRUCTURAL_LINK_TYPES
-#     :param all_links:
-#     """
-#     adj_list = []
-#     for tup in all_links.itertuples(index=False):
-#         for link_type in STRUCTURAL_LINK_TYPES:
-#             tup_ind = all_links.columns.get_loc(link_type)
-#             if len(tup[tup_ind]) > 0:
-#                 for link_url in tup[tup_ind]:
-#                     adj_list.append((tup.content_id, link_url, link_type))
-
-
-def make_structural_network(all_links_df):
-    # filter out any  links without a destination content ID, as we are building a network based on content_ids
-    logging.info(f'''
-        shape of all_links_df before dropping null 
-        destination_content_ids={all_links_df.shape}
-        ''')
+def make_structural_network_and_node_id_mappings(all_links_df):
+    logger = logging.getLogger('make_structural_network.make_structural_network_and_node_id_mappings')
+    # filter out any  inks without a destination content ID, as we are building a network based on content_ids
+    logger.info(
+        f'shape of all_links_df before dropping null destination_content_ids={all_links_df.shape}')
     all_links_df.query('destination_content_id.notnull()', inplace=True)
-    logging.info(f'''
-        shape of all_links_df after dropping null 
-        destination_content_ids={all_links_df.shape}
-        ''')
+    logger.info(
+        f'shape of all_links_df after dropping null  destination_content_ids={all_links_df.shape}')
 
     content_id_node_id_mapping = {}
     num_counter = 0
 
+    logger.info('assigning node_ids to content_ids, to create content_id_node_id_mapping')
     for i, row in all_links_df.iterrows():
         if row['source_content_id'] not in content_id_node_id_mapping.keys():
             content_id_node_id_mapping[row['source_content_id']] = num_counter
@@ -50,12 +33,43 @@ def make_structural_network(all_links_df):
     all_links_df['source'] = 0
     all_links_df['target'] = 0
 
+    logger.info('adding source and target node_ids to all_links_df')
     all_links_df['source'] = all_links_df['source_content_id'].map(
         content_id_node_id_mapping)
     all_links_df['target'] = all_links_df['destination_content_id'].map(
         content_id_node_id_mapping)
 
+    logger.info(
+        'creating node_id_content_id_mapping from content_id_node_id_mapping')
     node_id_content_id_mapping = dict(
         (v, k) for k, v in content_id_node_id_mapping.items())
 
     return all_links_df, content_id_node_id_mapping, node_id_content_id_mapping
+
+
+if __name__ == "__main__":  # our module is being executed as a program
+    datadir = os.getenv('DATADIR')
+    module_logger = logging.getLogger('make_structural_network')
+
+    module_logger.info(f'reading {datadir}/tmp/all_links.csv created by data_preprocessing/get_content_store_data')
+    input_df = pd.read_csv(os.path.join(datadir, 'tmp',  'all_links.csv'))
+
+    module_logger.info('making structural_network_df using all_links')
+    structural_network_df, content_id_node_id_mapping_dict, node_id_content_id_mapping_dict = \
+        make_structural_network_and_node_id_mappings(input_df)
+
+    module_logger.info(f'saving structural_network_df to {datadir}/tmp/structural_network.csv')
+    structural_network_df.to_csv(os.path.join(
+        datadir, 'tmp', 'structural_network.csv'), index=False)
+
+    module_logger.info(f'saving content_id_node_id_mapping_dict to {datadir}/tmp/content_id_node_id_mapping.json')
+    with open(
+            os.path.join(datadir, 'tmp', 'content_id_node_id_mapping.json'),
+            'w') as content_id_node_id_file:
+        json.dump(content_id_node_id_mapping_dict, content_id_node_id_file)
+
+    module_logger.info(f'saving node_id_content_id_mapping_dict to {datadir}/tmp/node_id_content_id_mapping.json')
+    with open(
+            os.path.join(datadir, 'tmp', 'node_id_content_id_mapping.json'),
+            'w') as node_id_content_id_file:
+        json.dump(node_id_content_id_mapping_dict, node_id_content_id_file)
--- tests/unit/features/test_make_structural_network.py
+++ tests/unit/features/test_make_structural_network.py
@@ -1,30 +1,13 @@
 import json
-import pytest
 
 import pandas as pd
 import pandas.testing as pd_testing
 
-from src.features.make_structural_network import *
-#
-#
-
-
-# @pytest.fixture(scope='module')
-# def all_links_test_sample_df():
-#     return pd.read_csv('tests/unit/fixtures/all_links_test_sample.csv')
-
-#
-# def test_str_array_to_array():
-#     assert str_array_to_array("['a','b']") == ['a', 'b']
-#
-#
-# def test_df_str_arrays_to_arrays():
-#     assert df_str_arrays_to_arrays(api_links_df, STRUCTURAL_LINK_TYPES) == pd.read]
-#
+from src.features.make_structural_network import make_structural_network_and_node_id_mappings
 
 
 def test_make_structural_network():
-    edges_df, content_id_node_id_mapping, node_id_content_id_mapping = make_structural_network(
+    edges_df, content_id_node_id_mapping, node_id_content_id_mapping = make_structural_network_and_node_id_mappings(
             pd.read_csv('tests/unit/fixtures/all_links_test_sample.csv'))
 
     pd_testing.assert_frame_equal(
