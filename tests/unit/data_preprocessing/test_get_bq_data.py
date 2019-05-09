734183d923dbd4e2996e355ccb5b3d5f32b9ef0e
Ellie King <ellie.king@digital.cabinet-office.gov.uk>
2019-05-09 21:08:55 +0100

    9 tests passing

---
+++ tests/unit/data_preprocessing/test_get_bq_data.py
@@ -0,0 +1,14 @@
+
+def test_retrieve_data_from_big_query():
+    # previous call for 2 weeks data cost $0.12
+    return pd.read_gbq(QUERY,
+                        project_id=PROJECT_ID,
+                        reauth=False,
+                        private_key=BQ_KEY,
+                        dialect="standard")
+
+
+# TODO only save out columns needed downstream
+# output_df.to_csv(os.path.join(DATA_DIR, "tmp",  "functional_network_data.csv"), index=False)
+
+
