91807397363ddac0dd41a0420e1a26e50ab34ada
Ellie King <ellie.king@digital.cabinet-office.gov.uk>
2019-05-15 21:12:43 +0100

    Convert functional_edges_and_weights to class and test

--- src/data_preprocessing/make_functional_edges_and_weights.py
+++ src/data_preprocessing/make_functional_edges_and_weights.py
@@ -4,30 +4,44 @@ from src.utils.miscellaneous import get_excluded_document_types, read_query
 import google.auth
 from google.cloud import bigquery
 
+
 # Need to have active environment variable called GOOGLE_APPLICATION_CREDENTIALS pointing to json file with
 # bigquery credentials
 
-if __name__ == "__main__":
 
-    data_dir = os.getenv("DATA_DIR")
-    blacklisted_document_types = ",".join(get_excluded_document_types()).split(",")
-    yesterday = (datetime.today() - timedelta(1)).strftime('%Y%m%d')
-    three_weeks_ago = (datetime.today() - timedelta(22)).strftime('%Y%m%d')
-
+class EdgeWeightExtractor:
     credentials, project_id = google.auth.default()
     client = bigquery.Client(
         credentials=credentials,
         project=project_id
     )
+    query_edge_list = read_query("src/data_preprocessing/query_content_id_edge_weights.sql")
 
-    query_config = bigquery.QueryJobConfig(
-        query_parameters=[
-            bigquery.ScalarQueryParameter("three_weeks_ago", "STRING", three_weeks_ago),
-            bigquery.ScalarQueryParameter("yesterday", "STRING", yesterday),
-            bigquery.ArrayQueryParameter("excluded_document_types", "STRING", blacklisted_document_types)
-        ]
-    )
+    def __init__(self, blacklisted_document_types, date_from, date_until):
+        self.blacklisted_document_types = blacklisted_document_types
+        self.date_from = date_from
+        self.date_until = date_until
 
-    query_edge_list = read_query("content_id_edge_weights.sql")
-    df = client.query(query_edge_list, job_config=query_config).to_dataframe()
-    df.to_csv(os.path.join(data_dir, "tmp", "functional_edges_dict.csv.gz"), index=False)
+        self.query_config = bigquery.QueryJobConfig(
+            query_parameters=[
+                bigquery.ScalarQueryParameter("three_weeks_ago", "STRING", self.date_from),
+                bigquery.ScalarQueryParameter("yesterday", "STRING", self.date_until),
+                bigquery.ArrayQueryParameter("excluded_document_types", "STRING", self.blacklisted_document_types)
+            ]
+        )
+
+        self.df = self.client.query(self.query_edge_list, job_config=self.query_config).to_dataframe()
+
+    def write_out_data_frame(self, file_path):
+        self.df.to_csv(file_path,
+                       index=False)
+
+
+if __name__ == "__main__":
+    data_dir = os.getenv("DATA_DIR")
+    blacklisted_document_types = ",".join(get_excluded_document_types()).split(",")
+    yesterday = (datetime.today() - timedelta(1)).strftime('%Y%m%d')
+    three_weeks_ago = (datetime.today() - timedelta(22)).strftime('%Y%m%d')
+
+    edge_weights = EdgeWeightExtractor(blacklisted_document_types, three_weeks_ago, yesterday)
+    edge_weights.write_out_data_frame(os.path.join(data_dir, "tmp", "functional_edges_dict.csv.gz"), compression='gzip')
---
+++ tests/unit/data_preprocessing/test_make_functional_edges_and_weights.py
@@ -0,0 +1,27 @@
+import pandas.testing as pd_testing
+import pandas as pd
+import pytest
+from src.data_preprocessing.make_functional_edges_and_weights import *
+from src.utils.miscellaneous import get_excluded_document_types
+
+
+@pytest.fixture
+def list_of_excluded_doctypes():
+    return ",".join(get_excluded_document_types()).split(",")
+
+@pytest.fixture
+def expected_edge_and_weights():
+    return pd.read_csv("tests/unit/fixtures/test_edge_and_weights_20190512.csv").reset_index(
+                                      drop=True)
+
+def test_return_data_frame(list_of_excluded_doctypes, expected_edge_and_weights):
+    instance = EdgeWeightExtractor(list_of_excluded_doctypes, "20190512", "20190512")
+    pd.set_option('display.max_colwidth', -1)
+    merged = instance.df.merge(expected_edge_and_weights,
+                               on=['source_node', 'destination_node', 'weight'],
+                               indicator=True,
+                               how='outer')
+
+    print(merged[merged['_merge'] != 'both'])
+    print(merged[merged['_merge'] != 'both'].shape[0])
+    assert merged[merged['_merge'] != 'both'].shape[0]==0
