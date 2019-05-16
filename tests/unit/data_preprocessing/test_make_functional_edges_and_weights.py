061c358087ed47c21e9c11c30f4efa9d195e7bab
Ellie King <ellie.king@digital.cabinet-office.gov.uk>
2019-05-16 13:12:10 +0100

    Skip test that calls BQ but keep code unless it's needed in future

--- tests/unit/data_preprocessing/test_make_functional_edges_and_weights.py
+++ tests/unit/data_preprocessing/test_make_functional_edges_and_weights.py
@@ -10,7 +10,7 @@ def expected_edge_and_weights():
     return pd.read_csv("tests/unit/fixtures/test_edge_and_weights_20190512.csv").reset_index(
         drop=True)
 
-
+@pytest.mark.skip(reason="Don't run up a big big query bill")
 def test_return_data_frame(expected_edge_and_weights):
     instance = EdgeWeightExtractor(get_excluded_document_types(), "20190512", "20190512")
     pd.set_option('display.max_colwidth', -1)
