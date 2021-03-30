a04e9c93b1ee9c6afcb3ae55d0b3eab2abf286b6
Suganya Sivaskantharajah <suganya.sivaskantharajah@digital.cabinet-office.gov.uk>
2021-03-30 12:40:36 +0100

    hardcoding a model dir instead of using env variable to check if tests pass

--- tests/unit/models/test_save_model.py
+++ tests/unit/models/test_save_model.py
@@ -6,9 +6,15 @@ from src.utils.epoch_logger import EpochLogger
 from src.models.train_node2vec_model import N2VModel
 from gensim.models import Word2Vec
 
+import logging.config
+
+
 # set seed for deterministic tests etc
 random.seed(2)
 np.random.seed(2)
+
+logging.config.fileConfig('src/logging.conf')
+module_logger = logging.getLogger('test_save_model')
 
 
 def test_weighted_node2vec(weighted_graph_config_fixture):
@@ -17,7 +23,10 @@ def test_weighted_node2vec(weighted_graph_config_fixture):
     Trains model, saves, loads and checks the model vocab is as trained
     """
     w_config = weighted_graph_config_fixture
-    model_dir = os.getenv("MODEL_DIR")
+
+    # trying a hardcoded model directory to see if the test will pass
+    # model_dir = os.getenv("MODEL_DIR")
+    model_dir = os.path.join(os.getcwd(), "tests/unit/tmp")
 
     # Node 5 to node 4 has zero weight (zero transition probability)
     # Node 4 to node 5 has ten weight (high transition probability)
@@ -31,6 +40,8 @@ def test_weighted_node2vec(weighted_graph_config_fixture):
                           'destination_content_id': destination_ids,
                           'weight': weights}
                          )
+
+
     wm = N2VModel()
 
     wm.create_graph(edges, w_config['weighted_graph'])
@@ -39,13 +50,21 @@ def test_weighted_node2vec(weighted_graph_config_fixture):
 
     wm.fit_model(**w_config, callbacks=EpochLogger())
 
+    module_logger.info(f'model_dir is {model_dir}')
+
     node_embeddings_file_path = os.path.join(model_dir,
                                              w_config['embeddings_filename'])
 
     node2vec_model_file_path = os.path.join(model_dir, w_config['model_filename'])
 
+    module_logger.info(f'node2vec_model_file_path is {node2vec_model_file_path}')
+
     wm.save_model(node_embeddings_file_path, node2vec_model_file_path)
 
     trained_model = Word2Vec.load(os.path.join(model_dir, w_config['model_filename']))
+
+    module_logger.info(f'vocab is {set(trained_model.wv.vocab.keys())}')
+    module_logger.info(f'expected vocab is {vocab}')
+
     print(set(trained_model.wv.vocab.keys()))
     assert set(map(int, trained_model.wv.vocab.keys())) == vocab
